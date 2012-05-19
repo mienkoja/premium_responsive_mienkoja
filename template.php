430c8de66f3ff7ae64fc1749e1a074b1dc59833a
Pradeep Saran <saran.quardz@gmail.com>
2012-05-20 05:14:38 +0530

    Issue #1590208: Missing meta viewport is added. Responsive is working fine and tested well.

--- template.php
+++ template.php
@@ -114,6 +114,19 @@ function responsive_preprocess_node(&$variables) {
   }
 }
 
+function responsive_page_alter($page) {
+  // <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+  $viewport = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#attributes' => array(
+    'name' =>  'viewport',
+    'content' =>  'width=device-width, initial-scale=1, maximum-scale=1'
+    )
+  );
+  drupal_add_html_head($viewport, 'viewport');
+}
+
 /**
  * Add javascript files for front-page jquery slideshow.
  */
